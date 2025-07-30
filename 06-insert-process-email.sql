2e7f6caad66e437be59ba3a9bba08e9a502b4546
nisepulvedaa <ni.sepulvedaa@gmail.com>
2025-07-30 19:36:34 -0400

    Update 06-insert-process-email.sql

--- 06-insert-process-email.sql
+++ 06-insert-process-email.sql
@@ -8,7 +8,7 @@ INSERT INTO `dev_config_zone.process_email` (
     JSON '''
     [
         {
-        "email_to_0": "{{correos_destino}}",
+        "email_to_0": "{{correos_destinatarios}}",
         "email_subj_err": "Resultado Ejecución Proceso: process-{{nombre_proceso}} [ERROR]", 
         "email_subj_ok": "Resultado Ejecución Proceso: process-{{nombre_proceso}} [OK]", 
         "email_body_err": "Process : process-{{nombre_proceso}} de Arquetipo: workflow-arquetipo-ingesta-cleansed Ejecutado Con Errores!", 
